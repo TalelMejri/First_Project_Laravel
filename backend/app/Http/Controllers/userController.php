5c27015955134bdcedc0c181750a6ea937286d0b
talel <talelmejri8@gmail.com>
2022-10-14 18:28:52 +0100

    first commit

---
+++ backend/app/Http/Controllers/userController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+/***add link */
+use App\Models\user_made;
+class userController extends Controller
+{
+
+    public function create(Request $request){
+        $user=user_made::create(
+            [
+                "name"=>$request->input("name"),
+                "email"=>$request->input("email"),
+            ]
+        );
+        return  response()->json(["data"=>$user],200);
+    }
+
+    public function get_all(){
+        $user=user_made::all();
+        if(!$user){
+            return response()->json(["message"=>"user nor found"]);
+        }
+        return response()->json(["data"=>$user],200);
+    }
+
+
+    /*public function updateuser  (Request $request, int $id)
+    {
+        $humane = Humans::find($id);
+        $humane->first_name = $request->first_name;
+        $humane->last_name = $request->last_name;
+        $humane->dob = $request->dob;
+        $humane->save();
+        return $humane;
+    }*/
+
+    public function update(Request $request,int $id){
+        $user=user_made::find($id);
+        if(!$user){
+            return response()->json(["message"=>"user not found"],404);
+        }else{
+            $user->update(
+                [
+                    "name"=>$request->input('name'),
+                    "email"=>$request->input('email'),
+                ],
+            );
+            return response()->json(["data"=>$user],200);
+        }
+    }
+
+    public function destroy(int $id){
+        $user=user_made::find($id);
+        if(!$user){
+            return response()->json(["message"=>"user not found"], 404);
+        }
+        $user->delete();
+        return  response()->json(["message"=>"User delete successfully"], 200);
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+/***add link */
+use App\Http\Controllers\userController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(['prefix' => '/user_made'], function () {
+    Route::get('/',[userController::class,'get_all']);
+    Route::post('/',[userController::class,'create']);
+    Route::put('/{id}',[userController::class,'update']);
+    Route::delete('/{id}',[userController::class,'destroy']);
+});
