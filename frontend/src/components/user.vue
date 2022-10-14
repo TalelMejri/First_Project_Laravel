3f8eb633114030b544e5b0f8f6e29ea6f3e2080a
talel <talelmejri8@gmail.com>
2022-10-14 20:41:05 +0100

    add part front

---
+++ frontend/src/components/user.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user">
+      Lorem ipsum dolor sit amet.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user',
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+      <page_user></page_user>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import page_user from '@/components/user.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    page_user
+  }
+}
+</script>
